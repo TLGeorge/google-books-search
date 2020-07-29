ac58a8983997f4a347763d08f8a93e575c4af6f2
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-07-28 22:16:00 -0400

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BarNav from './components/Navbar/navbar';
 import { Search, Saved, NoMatch, } from './pages'
 import GBJumbotron from "./components/Jumbotron/jumbotron";
-
 
 function App() {
   return (
     <div className="App">
       <BarNav />
       <GBJumbotron />
-      <Switch>
-        <Route path='/search'>
-          <Search />
-        </Route>
-        <Route path='/saved'>
-          <Saved />
-        </Route>
-        {/* <Route path='/details'>
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/saved'>
+            <Saved />
+          </Route>
+          {/* <Route path='/details'>
           <Details />
         </Route> */}
-        <Route path='/nomatch'>
-          <NoMatch />
-        </Route>
-      </Switch>
+          <Route path='/nomatch'>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 
 export default App;
